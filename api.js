4e603a6392781e79c33798e828da4b821d49f81e
pratiksulakshanemodern <pratik_sulakshane_mca@moderncoe.edu.in>
2025-11-04 14:59:35 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
-<script>  
-  let name="Pratik Sulakshane";
-  </script>
+// This goes inside /api/api.js
+export default function handler(req, res) {
+  const name = req.query.name || "World";
+  res.status(200).json({ message: `Hello, ${name}!` });
+}
